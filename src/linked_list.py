e3536e690ccd3bfb443a49c381380b696cdac09f
alexraum <maraum@ncsu.edu>
2022-08-20 00:55:06 -0400

    Added is_empty method

--- src/linked_list.py
+++ src/linked_list.py
@@ -11,3 +11,18 @@ class LinkedList:
 
     def __init__(self, data=None):
         self.front = Node(data)
+
+    def size(self):
+        if self.front.data == None:
+            return 0
+
+        count = 1
+        curr = self.front
+        while curr.next is not None:
+            count += 1
+            curr = curr.next
+
+        return count
+
+    def is_empty(self):
+        return self.size() == 0
