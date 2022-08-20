6bf5bb1ccf3641f77315ffd8f4610df9ccd77f80
alexraum <maraum@ncsu.edu>
2022-08-20 03:04:11 -0400

    Added get method

--- src/linked_list.py
+++ src/linked_list.py
@@ -15,12 +15,22 @@ class LinkedList:
     def size(self):
         if self.front.data == None:
             return 0
-        count = 1
         curr = self.front
-        while curr.next is not None:
-            count += 1
+        count = 0
+        while curr is not None:
             curr = curr.next
+            count += 1
         return count
 
     def is_empty(self):
         return self.size() == 0
+
+    def get(self, idx):
+        if ((idx < 0) or (idx > (self.size()-1))):
+            raise IndexError
+        curr = self.front
+        count = 0
+        while count != idx:
+            curr = curr.next
+            count += 1
+        return curr.data
