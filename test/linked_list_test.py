5c5cce27578ed1541b03feb87141f033ff4f9655
alexraum <maraum@ncsu.edu>
2022-08-20 00:55:33 -0400

    Added test_is_empty

--- test/linked_list_test.py
+++ test/linked_list_test.py
@@ -1,3 +1,24 @@
 """Defines unit tests for linked_list.py"""
+from src.linked_list import (
+    Node,
+    LinkedList
+)
 
-print("test")
+
+def test_size():
+    l1 = LinkedList()
+    l2 = LinkedList("apple")
+    l3 = LinkedList("apple")
+    n2 = Node("blueberry")
+    l3.front.next = n2
+
+    assert l1.size() == 0
+    assert l2.size() == 1
+    assert l3.size() == 2
+
+def test_is_empty():
+    l1 = LinkedList()
+    l2 = LinkedList("apple")
+
+    assert l1.is_empty() is True
+    assert l2.is_empty() is False
