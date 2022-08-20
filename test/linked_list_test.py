f30dce22eaf7e3df4b81cbc3645ba66ecc3fd663
alexraum <maraum@ncsu.edu>
2022-08-20 03:11:19 -0400

    Added test case

--- test/linked_list_test.py
+++ test/linked_list_test.py
@@ -3,22 +3,33 @@ from src.linked_list import (
     Node,
     LinkedList
 )
+import pytest
 
 
 def test_size():
-    l1 = LinkedList()
-    l2 = LinkedList("apple")
-    l3 = LinkedList("apple")
-    n2 = Node("blueberry")
-    l3.front.next = n2
+    list_1 = LinkedList()
+    list_2 = LinkedList("apple")
+    list_3 = LinkedList("apple")
+    node_1 = Node("blueberry")
+    list_3.front.next = node_1
 
-    assert l1.size() == 0
-    assert l2.size() == 1
-    assert l3.size() == 2
+    assert list_1.size() == 0
+    assert list_2.size() == 1
+    assert list_3.size() == 2
 
 def test_is_empty():
-    l1 = LinkedList()
-    l2 = LinkedList("apple")
+    list_1 = LinkedList()
+    list_2 = LinkedList("apple")
 
-    assert l1.is_empty() is True
-    assert l2.is_empty() is False
+    assert list_1.is_empty() is True
+    assert list_2.is_empty() is False
+
+def test_get():
+    list_1 = LinkedList("apple")
+    list_2 = LinkedList("apple")
+    node_1 = Node("blueberry")
+    list_2.front.next = node_1
+
+    assert list_1.get(0) == "apple"
+    assert list_2.get(0) == "apple"
+    assert list_2.get(1) == "blueberry"
