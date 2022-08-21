9285479b5522f91725a1a64c0c96a2927ac75dcd
alexraum <maraum@ncsu.edu>
2022-08-20 22:23:37 -0400

    Added test cases

---
+++ test/queue_test.py
@@ -0,0 +1,28 @@
+"""Defines unit tests for the queue abstract data type"""
+from src.queue import (
+    Queue
+)
+
+
+def test_size():
+    queue_1 = Queue()
+    queue_2 = Queue()
+    queue_2.enqueue("apple")
+    queue_2.enqueue("blueberry")
+
+    assert queue_1.size() == 0
+    assert queue_2.size() == 2
+
+def test_is_empty():
+    queue_1 = Queue()
+    queue_2 = Queue()
+    queue_2.enqueue("apple")
+
+    assert queue_1.is_empty() == True
+    assert queue_2.is_empty() == False
+
+def test_enqueue():
+    queue = Queue()
+    queue.enqueue("apple")
+
+    assert queue.size() == 1
